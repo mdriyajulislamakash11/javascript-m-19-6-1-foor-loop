124825eaee605f5b62c6010e94f9180fba92cbd7
akash ahmed <hafejmohammadakash@gmail..com>
2025-01-02 17:55:02 +0600

    done modiule

---
+++ index.js
@@ -0,0 +1,76 @@
+ //======= For loop__________________/
+//19_4 Introduction to For Loop.
+
+// ______________STRUCTURE__________________/
+for(let i = 1 ; i < 10 ; i++){
+      console.log(i)
+}
+
+//______________ Odd _____________/
+ 
+for(let i = 0; i < 50; i++){
+      if(i % 2 === 1){
+            console.log(i);  
+      };
+};
+console.log(sum);
+______________________________________________________/
+
+
+
+// ================== 19- 5 ================== //
+19_5 Problem solving with For Loop.
+// ___________DECREMENTAL Loop
+
+for(let i = 10; i >= 0; i--){
+      console.log(i)
+};
+
+
+// ___________INCREMENTAL Loop
+
+for(let i = 11; i <= 20; i++){
+      console.log(i)
+}
+
+
+
+// \_______________________ 19-6 ______________________/
+// \____________19_6 Different ways to use loop________/
+
+
+
+
+
+
+
+
+
+
+
+// \_______________________ 19-7 ______________________/
+// \____________9_7 When to use a break and continue________/
+ /______________breack________________________/
+
+for(let i = 1; i < 10; i++){
+      console.log(i);
+      if(i >= 5){
+            break;
+      }
+}
+
+/_____________ CONTINUE _____________/
+for(let i = 1; i < 10; i++){
+      // console.log(i);
+      if( i % 2 ===1){
+            continue;
+      }
+      console.log(i)
+}
+______________________________________________/
+
+
+
+
+
+
